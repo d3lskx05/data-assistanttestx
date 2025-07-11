0d95687dd1ccb2dacfcb2e34cb0489876f44db28
d3lskx05 <d3lskx@gmail.com>
2025-07-12 00:09:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if query:
                 if comment and str(comment).strip().lower() != "nan":
                     with st.expander("Комментарий"):
                         st.markdown(comment)
-               st.markdown("<hr style='margin:4px 0' />", unsafe_allow_html=True)
+                st.markdown("<hr style='margin:4px 0' />", unsafe_allow_html=True)
         else:
             st.info("Ничего не найдено в точном поиске.")
 
