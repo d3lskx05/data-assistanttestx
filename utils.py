762580ebc3732683f81f2b484b1520bf486f9dc9
d3lskx05 <d3lskx@gmail.com>
2025-07-12 12:12:41 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ for group in SYNONYM_GROUPS:
         SYNONYM_DICT[lemma] = lemmas
 
 GITHUB_CSV_URLS = [
-    "https://raw.githubusercontent.com/codxqqq/semantic-assistant/main/data4.xlsx",
+    "https://raw.githubusercontent.com/d3lskx05/data-assistanttestx/main/data4.xlsx",
     "https://raw.githubusercontent.com/skatzrsk/semantic-assistant/main/data21.xlsx",
     "https://raw.githubusercontent.com/skatzrsk/semantic-assistant/main/data31.xlsx"
 ]
